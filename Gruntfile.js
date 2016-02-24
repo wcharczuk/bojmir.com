39585616d18e86825fae3d4b4a957ad81b654b1d
Will Charczuk <will@clotheshor.se>
2016-02-24 12:24:02 -0800

    initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        aws: grunt.file.readJSON('aws.json'),
+
+        s3: {
+            options: {
+                accessKeyId: '<%= aws.AWSAccessKeyId %>',
+                secretAccessKey: '<%= aws.AWSSecretKey %>',
+                bucket: '<%= aws.AWS_S3_Bucket %>',
+                access: "public-read"
+            },
+            dist: {
+                cwd: "dist/",
+                src: "**"
+            }
+        },
+
+    });
+
+    grunt.loadNpmTasks('grunt-aws');
+
+    grunt.registerTask(
+        'deploy',
+        'Deploys the site to S3',
+        [ 's3:dist' ]
+    )
+};
